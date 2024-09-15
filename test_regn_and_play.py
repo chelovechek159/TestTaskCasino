a13b01f7e453b7022b5b918c4e46fab3e4331d77
yaroslav <yarikkomarik@gmail.com>
2024-09-15 19:41:33 +0300

    изменен код

--- test_regn_and_play.py
+++ test_regn_and_play.py
@@ -9,7 +9,6 @@ user_data = {
     'nickname': fake.user_name(),
     'email': fake.email(),
     'password': fake.password()
-
 }
 
 
@@ -21,66 +20,63 @@ def browser():
         browser.close()  # Закрываем браузер после завершения тестов
 
 
-def test_regn(browser):
-    page = browser.new_page()  # Создаём новую страницу
+def open__and_fill_registration_page(page):
     page.goto(url)
-
-    # Ждём появления кнопки регистрация
     page.wait_for_selector('div.SimpleButton:has-text("Sign up")')
     page.click('div.SimpleButton:has-text("Sign up")')  # Нажимаем кнопку регистрации
-
-    # имя пользователя
+    # Заполняем форму регистрации
     page.fill('input[type="text"][tabindex="0"].Input.with_clear_button', user_data['nickname'])
-    # email
     page.fill('input[type="email"]', user_data['email'])
-    # пароль
     page.fill('input[type="password"]', user_data['password'])
-    # подтверждение пароля
     page.fill('div.password_confirm-field-container input[type="password"]', user_data['password'])
-
     # Нажимаем кнопку регистрации
     page.click('div.panel.SimpleButton.SimpleButton_use_text.send-form')
 
-    # Нажимаем на кнопку для открытия sideBar menu
+
+def check_account_information(page):
+    # Открываем меню и проверяем информацию об аккаунте
     page.click('.menu_button.SimpleButton')
-    # Нажимаем на дропдаун Account
     page.click('.LobbySidebarContainer__action.account.SimpleButton:not(.isShowSubAction)')
-    # Нажать на кнопку Информация об аккаунте
     page.click('div.LobbySidebarContainer__action.account_info')
 
-    # Получаем отображаемые данные
     displayed_nickname = page.inner_text('div.AccountInformationContainer__form_field.name span')
     displayed_email = page.inner_text('div.AccountInformationContainer__form_field.email span')
 
-    # Проверяем, что отображаемые данные соответствуют отправленным данным
     assert displayed_nickname == user_data['nickname'], \
         f"Ожидалось {user_data['nickname']}, но отображается {displayed_nickname}"
     assert displayed_email == user_data['email'], f"Ожидалось {user_data['email']}, но отображается {displayed_email}"
 
     page.click('.Dialog__action.cancel')
 
+
+def check_player_balance(page):
+    # Проверяем, что баланс игрока отображается
+    assert page.is_visible('.LobbyHeaderContainer__expand_button'), "Баланс игрока не отображается"
+
+
+def launch_first_casino_game(page):
+    # Переход в раздел казино и запуск игры
     page.wait_for_selector('text="Casino"')
     page.click('text="Casino"')
 
     page.wait_for_selector('.WidgetCasinoGameListItemContainer')  # Ожидание появления игр
     first_game = page.query_selector('.WidgetCasinoGameListItemContainer')  # Находим первую ячейку игры
+    first_game.hover()  # Навести мышь на ячейку игры
 
-    # Навести мышь на ячейку игры и запустить игру
-    first_game.hover()
-
-    # Ожидание кнопки "Запустить игру"
     page.wait_for_selector('.WidgetCasinoGameListItemContainer__play')  # Ожидание кнопки запуска
-
-    # Клик по кнопке "Запустить игру"
     page.click('.WidgetCasinoGameListItemContainer__play')  # Нажимаем на кнопку "Запустить игру"
 
     page.wait_for_timeout(10000)
     # Проверяем, что игровое поле загружено
     assert page.is_visible('.WidgetCasinoGameListGamesPlayerItemContainer__game_widget_wrapper'), \
         "Игровое поле не отображается"
-    # Проверяем, что на странице нет сообщений об ошибке
     assert not page.is_visible('.error-message'), "Обнаружено сообщение об ошибке при запуске игры"
-    # Проверяем, что баланс игрока отображается
-    assert page.is_visible('.LobbyHeaderContainer__expand_button'), "Баланс игрока не отображается"
 
+
+def test_registration(browser):
+    page = browser.new_page()  # Создаём новую страницу
+    open__and_fill_registration_page(page)  # Открываем и заполняем страницу регистрации
+    check_account_information(page)  # Проверяем данные аккаунта
+    check_player_balance(page)  # Проверяем баланс
+    launch_first_casino_game(page)  # Запускаем игру в казино
     page.close()
